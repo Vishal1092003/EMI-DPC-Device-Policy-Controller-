6eeb9d2bd56b73249f0bef9f1014905b1b2ca93d
vishal <codewithme2615@gmail.com>
2025-10-26 21:22:22 +0530

    ip whitelisted 0.0.0.0/0 in mongo atlas

--- .history/backend/db_20251023103436.js
+++ .history/backend/db_20251023103436.js
@@ -5,9 +5,8 @@ dotenv.config();
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 10000, // 10s
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
         });
         console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
---
+++ .history/backend/index_20251026142542.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+const connectDB = require("./db");
+const deviceRoutes = require("./routes/deviceRoutes");
+const provisionRoutes = require("./routes/provisionRoutes");
+const adminRoutes = require("./routes/adminRoutes")
+const path = require("path");
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(cors({ origin: "*" })); // ✅ allow all origins (or restrict later)
+
+// ✅ serve static files (for APK hosting)
+app.use("/static", express.static(path.join(__dirname, "static")));
+app.get("/", (req, res) => res.send("📱 EMI Lock System Backend Running"));
+
+
+app.use("/api/devices", deviceRoutes);
+app.use("/api/provision", provisionRoutes);
+app.use("/api/admin", adminRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Server running on ${PORT}`));
